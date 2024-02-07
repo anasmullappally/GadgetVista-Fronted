b78bee9f4744615d24d5702a1c29f8a48624d21a
anasmullappally <mohammedanas362@gmail.com>
2024-02-07 19:34:37 +0530

    single product responsive

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -43,7 +43,12 @@ function SingleProduct() {
                     </div>
                 </div>
                 <div className="rating">
-                    <div className="rating-percentage"> 4.8</div>
+                    <div className="rating-percentage">
+                        <span>4.8</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 ml-1">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                        </svg>
+                    </div>
                     <div className="total-ratings">
                         539 ratings and 62 reviews
                     </div>
@@ -101,7 +106,12 @@ function SingleProduct() {
                 <div className="ratings-and-reviews">
                     <div className="current-rating">
                         <div className="heading">Rating and Reviews</div>
-                        <div className="rating-percentage"> 4.8</div>
+                        <div className="rating-percentage">
+                            <span>4.8</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 ml-1">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                            </svg>
+                        </div>
                         <div className="total-ratings">
                             539 ratings and 62 reviews
                         </div>
@@ -111,13 +121,22 @@ function SingleProduct() {
                         (<div className="single-review" key={index}>
                             <div className="name-and-rating" >
                                 <span className="name">Name {item}</span>
-                                <span className="rating">5</span>
+                                <div className="rating-count">
+                                    <span>4</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3 ml-1">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                                    </svg>
+                                </div>
                             </div>
                             <div className="comment-and-helpful">
                                 <div className="comment">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, assumenda minima. Nulla rem placeat, facilis totam, nisi sed assumenda fugit, modi perspiciatis accusantium alias harum ea molestiae incidunt reprehenderit in.</div>
                                 <div className="helpful">
-                                    <div className="like pointer">Like</div>
-                                    <div className="dislike pointer">Dislike</div>
+                                    <div className="like pointer">
+                                        <svg className="h-5 w-5 text-stone-400" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M7 11v 8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3" /></svg>
+                                    </div>
+                                    <div className="dislike pointer">
+                                        <svg className="h-5 w-5 text-stone-400" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <path d="M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v 1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3" /></svg>
+                                    </div>
                                 </div>
                             </div>
                         </div>)
