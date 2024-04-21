7c68a6b457716e0e091e33d7e95e151c1190cc97
Anas Mullappally <101805320+anasmullappally@users.noreply.github.com>
2024-04-21 11:59:13 +0530

    add dynamic title

--- src/pages/AdminHomePage.jsx
+++ src/pages/AdminHomePage.jsx
@@ -1,5 +1,6 @@
 import { BsFillArchiveFill, BsFillBellFill, BsFillGrid3X3GapFill, BsPeopleFill } from "react-icons/bs"
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } from 'recharts';
+import Title from "../components/common/Title";
 function AdminHomePage() {
     const data = [
         {
@@ -46,85 +47,88 @@ function AdminHomePage() {
         },
     ];
     return (
-        <div className="dashboard">
-            <div className="heading">DashBoard</div>
-            <div className="cards">
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3>PRODUCTS</h3>
-                        <BsFillArchiveFill className='card_icon' />
+        <>
+            <Title title={"Admin"} />
+            <div className="dashboard">
+                <div className="heading">DashBoard</div>
+                <div className="cards">
+                    <div className='card'>
+                        <div className='card-inner'>
+                            <h3>PRODUCTS</h3>
+                            <BsFillArchiveFill className='card_icon' />
+                        </div>
+                        <h1>300</h1>
                     </div>
-                    <h1>300</h1>
+                    <div className='card'>
+                        <div className='card-inner'>
+                            <h3>CATEGORIES</h3>
+                            <BsFillGrid3X3GapFill className='card_icon' />
+                        </div>
+                        <h1>12</h1>
+                    </div>
+                    <div className='card'>
+                        <div className='card-inner'>
+                            <h3>CUSTOMERS</h3>
+                            <BsPeopleFill className='card_icon' />
+                        </div>
+                        <h1>33</h1>
+                    </div>
+                    <div className='card'>
+                        <div className='card-inner'>
+                            <h3>ALERTS</h3>
+                            <BsFillBellFill className='card_icon' />
+                        </div>
+                        <h1>42</h1>
+                    </div>
                 </div>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3>CATEGORIES</h3>
-                        <BsFillGrid3X3GapFill className='card_icon' />
-                    </div>
-                    <h1>12</h1>
-                </div>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3>CUSTOMERS</h3>
-                        <BsPeopleFill className='card_icon' />
-                    </div>
-                    <h1>33</h1>
-                </div>
-                <div className='card'>
-                    <div className='card-inner'>
-                        <h3>ALERTS</h3>
-                        <BsFillBellFill className='card_icon' />
-                    </div>
-                    <h1>42</h1>
+                <div className='charts'>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <BarChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Bar dataKey="pv" fill="#8884d8" />
+                            <Bar dataKey="uv" fill="#82ca9d" />
+                        </BarChart>
+                    </ResponsiveContainer>
+
+                    <ResponsiveContainer width="100%" height="100%">
+                        <LineChart
+                            width={500}
+                            height={300}
+                            data={data}
+                            margin={{
+                                top: 5,
+                                right: 30,
+                                left: 20,
+                                bottom: 5,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        </LineChart>
+                    </ResponsiveContainer>
+
                 </div>
             </div>
-            <div className='charts'>
-                <ResponsiveContainer width="100%" height="100%">
-                    <BarChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey="pv" fill="#8884d8" />
-                        <Bar dataKey="uv" fill="#82ca9d" />
-                    </BarChart>
-                </ResponsiveContainer>
-
-                <ResponsiveContainer width="100%" height="100%">
-                    <LineChart
-                        width={500}
-                        height={300}
-                        data={data}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                    </LineChart>
-                </ResponsiveContainer>
-
-            </div>
-        </div>
+        </>
     )
 }
 
